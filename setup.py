957b0ee13ae2dae480ef28e3a53d7b92487d6dad
danielballan <daniel.b.allan@gmail.com>
2017-03-07 17:52:15 -0500

    MNT: Organize into a package.

---
+++ setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+import setuptools
+import versioneer
+
+
+setup(name='databroker-browser',
+      version=versioneer.get_version(),
+      cmdclass=versioneer.get_cmdclass(),
+      packages=['databroker_browser',
+                'databroker_browser.qt'],
+     )
